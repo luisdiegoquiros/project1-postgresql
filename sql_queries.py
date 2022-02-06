b4ca75a4170fd286e188cc72978cc09c8fc173f3
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2022-02-06 12:57:28 -0600

    Insertion in table songs

--- sql_queries.py
+++ sql_queries.py
@@ -72,7 +72,6 @@ user_table_insert = ("""
 """)
 
 song_table_insert = """INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)"""
-""")
 
 artist_table_insert = ("""
 """)
