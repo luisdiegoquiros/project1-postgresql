feaec045cdfb29c118c63ffa175f5d329c2bf336
Luis Diego Quirós Gómez <luis25quiros@gmail.com>
2022-02-06 16:18:11 -0600

    ETL in python file

--- etl.py
+++ etl.py
@@ -6,66 +6,127 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    Reads the data file and insert the corresponding
+    information in the songs and artists table.
+    Args:
+        cur: cursor connection to the database
+        filepath: path to the file with the information to add to the database.
+    """
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']]
+    song_data = song_data.values.tolist()
+    song_data = song_data[0]
+
+    # run query to insert in the table
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id',
+                      'artist_name',
+                      'artist_location',
+                      'artist_latitude',
+                      'artist_longitude']]
+    artist_data = artist_data.values.tolist()
+    artist_data = artist_data[0]
+
+    # run query to insert in the table
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """
+    Reads the log data file and insert the corresponding
+    information in the time, users and
+    song plays table.
+    Args:
+        cur: cursor connection to the database
+        filepath: path to the file with the information to add to the database.
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t.dt.time,
+                 t.dt.hour,
+                 t.dt.day,
+                 t.dt.isocalendar().week,
+                 t.dt.month, t.dt.year,
+                 t.dt.dayofweek]
+    column_labels = ('start_time',
+                     'hour',
+                     'day',
+                     'week',
+                     'month',
+                     'year',
+                     'weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId',
+                  'firstName',
+                  'lastName',
+                  'gender',
+                  'level']].drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
+    # Convert ts column to datetime for correct insertion on the table
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms').dt.time
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.sessionId,
+                         row.ts,
+                         row.userId,
+                         row.level,
+                         songid,
+                         artistid,
+                         row.sessionId,
+                         row.location,
+                         row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    Receives a path for a folder and loads all the files
+    using the given function
+    Args:
+        cur: cursor connection to the database
+        conn: connection to the database to add the records
+        filepath: path to the files to process
+        func: function to use to insert the file in the database
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -80,7 +141,14 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    """
+    Main function that calls the process data
+    to fill the database with the records
+    """
+    conn = psycopg2.connect("host=127.0.0.1 \
+                            dbname=sparkifydb \
+                            user=student \
+                            password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
